3704d4ac7a1643ec8ff0f2fea2fd4f3430763544
uoshvis <uoshvis@gmail.com>
2021-05-26 20:48:05 +0300

    moved settings to process initialization

--- src/initial_template/initial_template.py
+++ src/initial_template/initial_template.py
@@ -4,8 +4,8 @@ from scrapy.crawler import CrawlerProcess
 
 class MySpider(scrapy.Spider):
     name = 'scraper_name'
+    allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
-
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
     }
@@ -16,6 +16,9 @@ class MySpider(scrapy.Spider):
 
 if __name__ == '__main__':
     # run scraper
-    process = CrawlerProcess()
+    process = CrawlerProcess(settings={
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+        'CONCURRENT_REQUESTS': 2
+    })
     process.crawl(MySpider)
     process.start()
